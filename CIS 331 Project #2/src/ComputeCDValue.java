a1b6cf8bd87d46306246e01b98972233667ac37f
Tofig Gasimov <99315612+GasimovT@users.noreply.github.com>
2022-04-16 18:50:44 -0400

    Add files via upload

---
+++ CIS 331 Project #2/src/ComputeCDValue.java
@@ -0,0 +1,63 @@
+/*
+ * Tofig Gasimov
+ * Date: Fall 2020
+ *
+ * CIS 331
+ * My submission of this program indicates that I have neither received nor
+given substantial or unauthorized assistance in writing this program.
+*
+   Exercise 5.31-Financial application: Compute CD value
+ */
+
+/*
+ *
+ * @author tofig
+ */
+
+/*
+ (Financial application: compute CD value) Suppose you put $10,000 into a CD
+with an annual percentage yield of 6.15%. After one month, the CD is worth
+10000 + 10000 * 6.15 / 1200 = 10051.25
+ After two months, the CD is worth
+10051.25 + 10051.25 * 6.15 / 1200 = 10102.76
+and so on.
+Write a program that prompts the user to enter an amount (e.g., 10000), the annual
+percentage yield (e.g., 6.15), and the number of months (e.g., 18)
+
+ */
+
+import java.util.Scanner;
+
+public class ComputeCDValue {
+    public static void main(String[] args) {
+        //Creating Scanner object for input
+        Scanner scanner = new Scanner(System.in);
+
+        //Prompting user to enter values and reading those values
+
+        System.out.println("Enter the initial deposit amount: ");
+        double depositAm = scanner.nextInt();
+
+        System.out.println("Enter annual percentage yield: ");
+        double annualYield = scanner.nextDouble();
+
+        System.out.println("Enter maturity period (number of months): ");
+        int maturityMon = scanner.nextInt();
+
+        //Calculating monthly yield %
+        double monthlyYield = annualYield / 1200;
+
+        //Displaying Results
+        System.out.println(" ");
+        System.out.println("Month\t" + "CD Value");
+        for (int i = 1; i <= maturityMon; i++) {
+
+            //Calculating CD Value for each month
+            double cdValue = depositAm + (depositAm * monthlyYield);
+
+            //Incrementing deposit amount for each month
+            depositAm += (depositAm * monthlyYield);
+            System.out.println(i + "\t\t" + String.format("%.2f", cdValue));
+        }
+    }
+}
---
+++ CIS 331 Project #2/src/PrintingDistinctNumbers.java
@@ -0,0 +1,78 @@
+/*
+ * Tofig Gasimov
+ * Date: Fall 2020
+ *
+ * CIS 331
+ * My submission of this program indicates that I have neither received nor
+given substantial or unauthorized assistance in writing this program.
+*
+   Exercise 7.5-Printing distinct numbers
+ */
+
+/*
+ *
+ * @author tofig
+ */
+
+
+/*
+(Print distinct numbers) Write a program that reads in ten numbers and displays the number of distinct numbers and
+the distinct numbers separated by exactly one space (i.e., if a number appears multiple times, it is displayed only
+once). (Hint: Read a number and store it to an array if it is new. If the number is already in the array, ignore it.)
+After the input, the array contains the distinct numbers. Here is the sample run of the program:
+Enter ten numbers: 1 2 3 2 1 6 3 4 5 2
+The number of distinct number is 6
+The distinct numbers are: 1 2 3 6 4 5
+ */
+
+import java.util.Scanner;
+
+public class PrintingDistinctNumbers {
+    public static void main(String[] args) {
+
+        //Creating scanner object for input
+        Scanner scanner = new Scanner(System.in);
+
+        //Declaring two arrays
+        int[] array = new int[10];
+        int[] distinctArray = new int[10];
+
+        //Prompting user to enter numbers
+        //Reading numbers into array
+        System.out.print("Enter ten numbers: ");
+        for (int i = 0; i < array.length; i++) {
+            array[i] = scanner.nextInt();
+        }
+
+        //Integer variable to increment array index
+        int index = 0;
+
+        // Looping through first array
+        for (int i = 0; i < array.length; i++) {
+            int flag = 0;
+            //Innermost loop to make comparison between elements and skip duplicate elements
+            for (int j = 0; j < i; j++) {
+                if (array[i] == array[j]) {
+                    flag = 1;
+                    break;
+                }
+            }
+            //Store the element if it is not present in the distinctArray
+            if (flag == 0) {
+                distinctArray[index] = array[i];
+                index++;    //increment index value
+            }
+        }
+
+        //Displaying Results
+        System.out.println("The number of distinct number is " + index);
+        System.out.print("The distinct numbers are: ");
+        for (int i = 0; i < index; i++) {
+            System.out.print(distinctArray[i] + " ");
+        }
+
+
+    }
+}
+
+
